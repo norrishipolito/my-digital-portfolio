54ede461e1eb13a964fd0bdd4a5dc5f39e6682f9
NORRIS HIPOLITO <NORRISHIPOLITO@example.com>
2022-08-04 21:59:43 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/Layout/NavBar/NavBar';
+import { ChakraProvider, Box, Spacer } from '@chakra-ui/react'
+import Footer from './Components/Layout/Footer/Footer';
+import LandingPage from './Components/Layout/Content/LandingPage';
+import DepartmentAssignment from './Components/Layout/Content/DepartmentAssignment';
+import WeeklyActivities from './Components/Layout/Content/WeeklyActivities';
+import Company from './Components/Layout/Content/Company';
+import CompanyProfile from './Components/Layout/Content/CompanyProfile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <NavBar />
+        <Box bg="#406882">
+          <LandingPage />
+          <Company/>
+          <CompanyProfile/>
+          <DepartmentAssignment/>
+          <WeeklyActivities/>
+        </Box>
+        <Footer />
+      </ChakraProvider>
     </div>
   );
 }
---
+++ src/Components/Layout/Content/DepartmentAssignment.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import {
+  Box,
+  Heading,
+  Link,
+  Image,
+  Text,
+  Divider,
+  HStack,
+  Tag,
+  Wrap,
+  WrapItem,
+  SpaceProps,
+  useColorModeValue,
+  Container,
+  VStack,
+} from '@chakra-ui/react';
+let lex_logo = require("../../../Images/lex-logo.jpg")
+interface IBlogTags {
+  tags: Array<string>;
+  marginTop?: SpaceProps['marginTop'];
+}
+
+const BlogTags: React.FC<IBlogTags> = (props) => {
+  return (
+    <HStack spacing={2} marginTop={props.marginTop}>
+      {props.tags.map((tag) => {
+        return (
+          <Tag size={'md'} variant="solid" colorScheme="orange" key={tag}>
+            {tag}
+          </Tag>
+        );
+      })}
+    </HStack>
+  );
+};
+
+interface BlogAuthorProps {
+  date: Date;
+  name: string;
+}
+
+export const BlogAuthor: React.FC<BlogAuthorProps> = (props) => {
+  return (
+    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
+      <Image
+        borderRadius="full"
+        boxSize="40px"
+        src={lex_logo}
+        alt={`Avatar of ${props.name}`}
+      />
+      <Text fontWeight="medium">{props.name}</Text>
+      <Text>—</Text>
+      <Text>{props.date.toLocaleDateString()}</Text>
+    </HStack>
+  );
+};
+
+const ArticleList = () => {
+  return (
+    <Container maxW={'7xl'} h='calc(100vh)' p="12">
+      <Heading as="h1">DEPARTMENT/DIVISION ASSIGNMENT</Heading>
+      <Box
+        marginTop={{ base: '1', sm: '5' }}
+        display="flex"
+        flexDirection={{ base: 'column', sm: 'row' }}
+        justifyContent="space-between">
+        <Box
+          display="flex"
+          flex="1"
+          marginRight="3"
+          position="relative"
+          alignItems="center">
+          <Box
+            width={{ base: '100%', sm: '85%' }}
+            zIndex="2"
+            marginLeft={{ base: '0', sm: '5%' }}
+            marginTop="5%">
+            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+              <Image
+                borderRadius="lg"
+                src={
+                  'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+                }
+                alt="some good alt text"
+                objectFit="contain"
+              />
+            </Link>
+          </Box>
+          <Box zIndex="1" width="100%" position="absolute" height="100%">
+            <Box
+              bgGradient={useColorModeValue(
+                'radial(orange.600 1px, transparent 1px)',
+                'radial(orange.300 1px, transparent 1px)'
+              )}
+              backgroundSize="20px 20px"
+              opacity="0.4"
+              height="100%"
+            />
+          </Box>
+        </Box>
+        <Box
+          display="flex"
+          flex="1"
+          flexDirection="column"
+          justifyContent="center"
+          marginTop={{ base: '3', sm: '0' }}>
+          <BlogTags tags={['Software Engineering', 'Product Delivery']} />
+          <Heading marginTop="1">
+            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+              Embedded Test Lab - Tools
+            </Link>
+          </Heading>
+          <Text
+            as="p"
+            marginTop="2"
+            color={useColorModeValue('gray.700', 'gray.200')}
+            fontSize="lg">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </Text>
+          <BlogAuthor name="Lexmark ETL" date={new Date('2021-04-06T19:01:27Z')} />
+        </Box>
+      </Box>
+      
+      <Divider marginTop="5" />
+      <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
+        <Heading as="h2"></Heading>
+        <Text as="p" fontSize="lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+        <Text as="p" fontSize="lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+    
+      </VStack>
+    </Container>
+  );
+};
+
+export default ArticleList;
